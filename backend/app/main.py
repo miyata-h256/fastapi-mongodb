707810d2f7a6c4f5621db7a4c2c619b2a3acb375
miyata-h256 <mhprivate79@gmail.com>
2023-02-14 23:09:58 +0900

    middleware setting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.server.routers import todolist
 from app.server.routers import todokind
 from app.server.routers import note
@@ -7,6 +8,18 @@ from app.server.database import init_db
 
 
 app = FastAPI(prefix="/")
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(todolist.router)
 app.include_router(todokind.router)
